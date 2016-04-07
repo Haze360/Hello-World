fef008973b704cf6e0f9d036871959466ab27896
Haze360 <ged129@yahoo.com>
2016-04-07 02:14:00 -0400

    Added files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+// Author: Haze360
+
+import javax.swing.JOptionPane;
+
+public class HelloWorld
+{
+   public static void main(String args[])
+   {
+      JOptionPane.showMessageDialog(null, "HELLO WORLD!!");
+   
+      System.exit(0);
+   }
+   
+   
+}
